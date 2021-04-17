cf130450de5db6bfcc835571de93cbe18e96598b
Your Name <you@example.com>
2021-04-17 06:28:48 +0000

    .

--- init.el
+++ init.el
@@ -1,2 +1,32 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
+;; Melpa
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-refresh-contents)
+(package-initialize)
+
+;; Use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(setq use-package-always-ensure t)
+
+;; Evil
+(use-package evil
+  :init      ;; tweak evil's configuration before loading it
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
+  (evil-mode))
+(use-package evil-collection
+  :after evil
+  :config
+  (setq evil-collection-mode-list '(dashboard dired ibuffer))
+  (evil-collection-init))
+
+(use-package general
+  :config
+  (general-evil-setup t))
