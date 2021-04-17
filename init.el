117a35f5293bdcb0d35fb145a5602d748e743114
Your Name <you@example.com>
2021-04-17 06:38:01 +0000

    .

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
+;; essentials
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+(global-set-key (kbd "C-c C-c") 'eval-region)
 
 ;; Melpa
 (require 'package)
@@ -27,6 +29,42 @@
   (setq evil-collection-mode-list '(dashboard dired ibuffer))
   (evil-collection-init))
 
+;; Themes
+
+;; Key bindings
 (use-package general
+  :ensure t
   :config
   (general-evil-setup t))
+
+(nvmap :states '(normal visual) :keymaps 'override :prefix "SPC"
+  "SPC"   '(counsel-M-x :which-key "M-x")
+  "."     '(find-file :which-key "Find file")
+  "f r"   '(counsel-recentf :which-key "Recent files")
+  "h r r" '((lambda () (interactive) (load-file "~/.emacs.d/init.el")) :which-key "Reload emacs config")
+  "t t"   '(toggle-truncate-lines :which-key "Toggle truncate lines"))
+
+
+
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-side-window-location 'bottom
+        which-key-sort-order #'which-key-key-order-alpha
+        which-key-sort-uppercase-first nil
+        which-key-add-column-padding 1
+        which-key-max-display-columns nil
+        which-key-min-display-lines 6
+        which-key-side-window-slot -10
+        which-key-side-window-max-height 0.25
+        which-key-idle-delay 0.8
+        which-key-max-description-length 25
+        which-key-allow-imprecise-window-fit t)
+  :config
+  (which-key-mode))
+
+
+(use-package auto-complete
+  :ensure t
+  :config
+  (ac-config-default))
